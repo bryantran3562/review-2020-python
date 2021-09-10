ae9d6b0807aec4c15603c83da057d1bf4c6d4d74
bryantran3562 <bryantran3562@gmail.com>
2021-09-10 08:46:19 -0500

    updated-9-10-21

--- step2-loops/loops.py
+++ step2-loops/loops.py
@@ -9,10 +9,12 @@ if __name__ == '__main__':
 
     mydict = {"name": "trong", "age": 24}
 
+    #BT - For the Python dict, you must use items()
     for k,v in mydict.items():
         print(k,v)
         
     my_array = [1,2,3,4,5]
     
+    #BT - For the list or array, use enumerate function.
     for i, item in enumerate(my_array):
         print(i, item)
